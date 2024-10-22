cb4325167cb1df1ad22c2d59c03fb515f5e5ee6d
jukr5-eadania <jukr5@student.eadania.dk>
2024-10-22 13:11:14 +0200

    Øvelse til og med 6 done

--- ObjektOrienteretProgrammering/OOP_D27/HighscoreList.cs
+++ ObjektOrienteretProgrammering/OOP_D27/HighscoreList.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 namespace OOP_D2
 {
-    internal class HighscoreList
+    internal static class HighscoreList
     {
-        private static Score[] scores = new Score[1];
+        private static int x = 0;
+        private static Score[] scores = new Score[10];
 
         internal static Score[] Scores { get => scores; set => scores = value; }
 
         public static void AddScore(Score score)
         {
-            Scores[0] = score;
+            if (x < scores.Length)
+            {
+                Scores[x] = score;
+                x++;
+            }
+            else
+            {
+                Array.Resize(ref scores, scores.Length * 2);
+            }
         }
 
         public static void PrintToScreen()
@@ -25,6 +34,7 @@ namespace OOP_D2
                 Console.Write(scores[i].Name);
                 Console.Write(" ");
                 Console.Write(scores[i].Points);
+                Console.WriteLine();
             }
         }
     }
--- ObjektOrienteretProgrammering/OOP_D27/Program.cs
+++ ObjektOrienteretProgrammering/OOP_D27/Program.cs
@@ -11,16 +11,36 @@ namespace OOP_D2
     {
         static void Main(string[] args)
         {
-            HighscoreList highscorelist = new HighscoreList();
-
             Score score1 = new Score("Jhonny", 300);
+            Score score2 = new Score("Mhonny", 400);
+            Score score3 = new Score("Thonny", 600);
+            Score score4 = new Score("Fhonny", 100);
+            Score score5 = new Score("Whonny", 200);
+            Score score6 = new Score("Shonny", 800);
+            Score score7 = new Score("Phonny", 900);
+            Score score8 = new Score("Lhonny", 3200);
+            Score score9 = new Score("Khonny", 2100);
+            Score score10 = new Score("Xhonny", 1200);
+            Score score11 = new Score("Bhonny", 3900);
 
             HighscoreList.AddScore(score1);
+            HighscoreList.AddScore(score2);
+            HighscoreList.AddScore(score3);
+            HighscoreList.AddScore(score4);
+            HighscoreList.AddScore(score5);
+            HighscoreList.AddScore(score6);
+            HighscoreList.AddScore(score7);
+            HighscoreList.AddScore(score8);
+            HighscoreList.AddScore(score9);
+            HighscoreList.AddScore(score10);
+            HighscoreList.AddScore(score11);
+
 
             HighscoreList.PrintToScreen();
 
             Console.WriteLine();
 
+            
             Animal animal1 = new Animal("Biboo", 50, 2, 20);
 
             animal1.Eat();
