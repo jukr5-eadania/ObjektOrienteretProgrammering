2c76c410da4cc08d0b9bc186591eb0056042429e
jukr5-eadania <jukr5@student.eadania.dk>
2024-10-23 10:42:40 +0200

    Task 1 done

--- ObjektOrienteretProgrammering/OOP_D3/Program.cs
+++ ObjektOrienteretProgrammering/OOP_D3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,34 @@ namespace OOP_D3
     {
         static void Main(string[] args)
         {
+            List<string> enemies = new List<string>(10);
+            enemies.Add("Skeleton");
+            enemies.Add("Zombie");
+            enemies.Add("Elemental");
+            enemies.Add("Spider");
+            enemies.Add("Cultist");
+            enemies.Add("Dragon");
+            enemies.Add("Serpent");
+            enemies.Add("Sorcerer");
+            enemies.Add("Jimbo");
+            enemies.Add("Imp");
+
+            foreach (var item in enemies)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            enemies.RemoveAt(7);
+            enemies.Remove("Spider");
+            enemies.RemoveRange(2,1);
+
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                Console.WriteLine(enemies[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
